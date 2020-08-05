a578eb67c7c971de207027fd02e9df1bb58a2180
Hachem Said <labsar.ahmed@outlook.fr>
2020-08-05 15:03:32 -0400

    added schema to create the database and tables

---
+++ schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE database company_db;
+
+USE company_db;
+
+CREATE TABLE department (
+  id INT(10) NOT NULL AUTO_INCREMENT,
+  name varchar(30),
+  PRIMARY KEY (id)
+);
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title varchar(30) NOT NULL,
+  salary int(10) NOT NULL,
+  department_id INT(10),
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name varchar(30) NOT NULL,
+  last_name varchar(30) NOT NULL,
+  role_id INT(10) NOT NULL,
+  manager_id INT(10) NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id) REFERENCES role(id)
+);
